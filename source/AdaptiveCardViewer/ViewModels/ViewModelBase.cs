fd8cdb1ad7ab7e3a5c44ef6c1c1411a5b322d3b5
Tom Laird-McConnell <thermous@iciclecreek.com>
2023-10-17 16:26:48 -0700

    initial checkin

---
+++ source/AdaptiveCardViewer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AdaptiveCardViewer.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ source/AdaptiveCards.Rendering.Avalonia/class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AdaptiveCards.Rendering.Avalonia
+{
+    public class Class1
+    {
+
+    }
+}
