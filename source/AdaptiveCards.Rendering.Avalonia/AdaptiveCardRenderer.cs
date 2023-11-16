8600acc195915c1f6ce73cb7055e029d4b46054e
Tom Laird-McConnell <thermous@iciclecreek.com>
2023-11-15 16:46:19 -0800

    remove unused namespace

--- source/AdaptiveCards.Rendering.Avalonia/AdaptiveCardRenderer.cs
+++ source/AdaptiveCards.Rendering.Avalonia/AdaptiveCardRenderer.cs
@@ -6,7 +6,6 @@ using Avalonia.Controls;
 using Avalonia.Media;
 using System;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
